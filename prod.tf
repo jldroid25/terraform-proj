b8796d4cfe30510a55467029179fec1380804fff
JAMES L <37844922+jldroid25@users.noreply.github.com>
2020-12-30 19:42:04 -0500

    Added Load balancer

--- prod.tf
+++ prod.tf
@@ -14,6 +14,23 @@ resource "aws_s3_bucket" "prod_tf_course" {
 # we need a vpc  yes we can define on but lets use the aws default one. 
 resource "aws_default_vpc" "default" {}
 
+# Define our subnet availability zone 1 in N.Virginia region
+resource "aws_default_subnet" "default_az1"{
+    availability_zone = "us-east-1a"
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+# Define our subnet availability zone 2 in N.Virginia region
+resource "aws_default_subnet" "default_az2"{
+    availability_zone = "us-east-1b"
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+
 #adding a security group resources & rules.
 resource "aws_security_group" "prod_web" {
     name        = "prod_web" 
@@ -23,7 +40,7 @@ resource "aws_security_group" "prod_web" {
         from_port    = 80
         to_port      = 80
         protocol     = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] 
+        cidr_blocks  = ["0.0.0.0/0"] 
     }
 #rule for incoming traffic to server we could also use our IP address as well
       ingress {
@@ -45,27 +62,79 @@ resource "aws_security_group" "prod_web" {
     }
 }
 
-# Define our instance/server nginx & our security group
-resource "aws_instance" "prod_web" {
-    count = 2
+// # Define our instance/server nginx & our security group
+// resource "aws_instance" "prod_web" {
+//   count = 2
 
-    ami           = "ami-049f3725664f54adb"
-    instance_type = "t2.micro"
+//     ami           = "ami-049f3725664f54adb"
+//     instance_type = "t2.micro"
+//     tags = {
+//         "Terraform" : "true"
+//     }
+// }
+
+// # Associate & allocate our Elastic IP to our instance
+// # This will decouple our EIP , allow flexibility in scaling up
+// resource "aws_eip_association" "prod_web" {
+//     instance_id    = aws_instance.prod_web.0.id 
+//     allocation_id  = aws_eip.prod_web.id
+// }
+
+// # Create an Elastic IP
+// resource "aws_eip" "prod_web" {
+//      tags = {
+//         "Terraform" : "true"
+//     }
+// }
+
+#Define our Elastic Load Balancer  elb
+resource "aws_elb" "prob_web" {
+    name            = "prod-web-lb"
+    #instances      = aws_instance.prod_web.*.id
+    subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    security_groups = [aws_security_group.prod_web.id]
+
+    listener{
+        instance_port     = 80
+        instance_protocol = "http"
+        lb_port           = 80
+        lb_protocol       = "http"
+    }
     tags = {
         "Terraform" : "true"
     }
 }
 
-# Associate & allocate our Elastic IP to our instance
-# This will decouple our EIP , allow flexibility in scaling up
-resource "aws_eip_association" "prod_web" {
-    instance_id  = aws_instance.prod_web.0.id 
-    allocation_id  = aws_eip.prod_web.id
-}
-
-# Create an Elastic IP
-resource "aws_eip" "prod_web" {
-     tags = {
+# Define an Auto Scaling Group
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-049f3725664f54adb"
+  instance_type = "t2.micro"
+   tags = {
         "Terraform" : "true"
     }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["us-east-1a", "us-east-1b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id ] 
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform" 
+    value               = "true"
+    propagate_at_launch = true 
+  }
+ }
+
+# Create a new load balancer attachment
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
